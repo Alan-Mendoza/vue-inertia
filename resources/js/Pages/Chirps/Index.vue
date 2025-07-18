451403e0afce1f11181115100226d8909790704c
Alan <alanrodrigo.mg@gmail.com>
2025-07-18 12:32:29 -0400

    aprendiendo a llevar objetos y arrays a la vista desde defineProps

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,3 +1,11 @@
+<script setup>
+// const title = 'Title from Vue';
+    defineProps([
+        'subtitle',
+        'title'
+    ]);
+</script>
 <template>
-    <h1>Chirp Index</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,8 @@ Route::middleware([
 });
 
 Route::get('/chirps', function () {
-    return Inertia::render('Chirps/Index');
+    return Inertia::render('Chirps/Index')->with([
+        'subtitle' => 'From PHP to JS',
+        'title' => 'Title from Vue',
+    ]);
 })->name('chirps.index');
