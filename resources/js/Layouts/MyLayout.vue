787e6b26c17fb9bccf64ab7fb1a0414dda6cf01d
Alan <alanrodrigo.mg@gmail.com>
2025-07-18 13:21:54 -0400

    cambios de MyLayout

---
+++ resources/js/Layouts/MyLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <nav>NAV</nav>
+        <!-- <div class="bg-green-200">TITLE</div> -->
+        <div v-if="$slots.title" class="bg-green-200"><slot name="title" /></div>
+    </header>
+    <div class="bg-gray-200">
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,11 +1,16 @@
 <script setup>
 // const title = 'Title from Vue';
+import MyLayout from '@/Layouts/MyLayout.vue';
     defineProps([
         'subtitle',
         'title'
     ]);
 </script>
 <template>
-    <h1>{{ title }}</h1>
-    <h2>{{ subtitle }}</h2>
+    <MyLayout>
+        <!-- <template #title> -->
+            <h1>{{ title }}</h1>
+        <!-- </template> -->
+        <h2>{{ subtitle }}</h2>
+    </MyLayout>
 </template>
